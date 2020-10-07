e93f09d7564f309ac274bd02b110114be9d78ec4
Sergio Pereira <sergiohgp@gmail.com>
2020-10-07 13:59:16 -0400

    useCallBack and input implementations

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,40 +1,46 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <form action="">
-        <h1>Sign Up</h1>
+        <Form onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
 
-        <Input icon={FiUser} name="name" placeholder="Email" />
+          <Input icon={FiUser} name="name" placeholder="Name" />
 
-        <Input icon={FiMail} name="email" placeholder="Email" />
+          <Input icon={FiMail} name="email" placeholder="Email" />
 
-        <Input
-          icon={FiLock}
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
 
-        <Button type="submit">Register</Button>
-      </form>
+          <Button type="submit">Register</Button>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Back to login
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="login">
+          <FiArrowLeft />
+          Back to login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
